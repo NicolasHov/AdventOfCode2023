6db5e89e4caa2277151634ce14de9d1688ec7752
Nicolas Hovart <hovart.nicolas@gmail.com>
2023-12-06 14:21:46 +0100

    Day2 part2

--- day2.js
+++ day2.js
@@ -4,19 +4,19 @@ const input = require("fs")
     .split("\n")
     .map(x => x.substr(x.indexOf(':') + 1))
 const inputObj = { 0: 'empty', ...input }
-for (const iterator in inputObj) inputObj[iterator] = inputObj[iterator].split(";").map(x => x.split(","))
+for (const iterator in inputObj) inputObj[iterator] = inputObj[iterator].split(";").map(x => x.split(",").map(x => x.trim()))
 
 // solving
-const solve = (data) => {
+const getPossibleGames = (data) => {
     let listOfGamesToKeep = []
     for (const iterator in data) {
         let counter = 3 // we will decrement counter each time a case is out of scope
         data[iterator].map(games => {
             games
                 .map(game => { // scope : max 12 red cubes,max  13 green cubes, and max 14 blue cubes. 
-                    game.includes('red') ? game.match(/\d+/)[0] <= 12 ? null : counter-- : null
-                    game.includes('green') ? game.match(/\d+/)[0] <= 13 ? null : counter-- : null
-                    game.includes('blue') ? game.match(/\d+/)[0] <= 14 ? null : counter-- : null
+                    game.includes('red') ? parseFloat(game.match(/\d+/)[0]) <= 12 ? null : counter-- : null
+                    game.includes('green') ? parseFloat(game.match(/\d+/)[0]) <= 13 ? null : counter-- : null
+                    game.includes('blue') ? parseFloat(game.match(/\d+/)[0]) <= 14 ? null : counter-- : null
                 })
         })
         counter < 3 ? null : listOfGamesToKeep.push(parseFloat(iterator) + 1)
@@ -24,4 +24,37 @@ const solve = (data) => {
     return listOfGamesToKeep.reduce((acc, currVal) => acc + currVal, 0,);
 }
 
-console.log(solve(inputObj)) // NB: we could have used a regex like this const regex = new RegExp(/(\d+) (red|blue|green)/g)
+const getFewestCubes = (data) => {
+    let listOfFewestNumbers = []
+    for (const iterator in data) {
+        let maxRed = 0, maxGreen = 0, maxBlue = 0
+        data[iterator].map(games => {
+            console.log(games);
+            games
+                .map(game => {
+                    let value = parseFloat(game.match(/\d+/)[0])
+                    console.log("--value", value);
+                    game.includes('red') ? value > maxRed ? maxRed = value : null : null
+                    console.log("maxRed", maxRed);
+                    // console.log("gamematch:", value);
+                    game.includes('green') ? value > maxGreen ? maxGreen = value : null : null
+                    console.log("maxGreen", maxGreen);
+                    game.includes('blue') ? value > maxBlue ? maxBlue = value : null : null
+                    console.log("maxBlue", maxBlue);
+                    // console.log("blue:", value);
+
+                })
+        })
+        // console.log(maxRed);
+        // console.log(maxGreen);
+        // console.log(maxBlue);
+        listOfFewestNumbers.push(maxBlue * maxGreen * maxRed)
+        console.log("---------------------------");
+    }
+    console.log(listOfFewestNumbers);
+    return listOfFewestNumbers.reduce((acc, currVal) => acc + currVal, 0,);
+}
+
+
+console.log(getFewestCubes(inputObj))
+// console.log(getPossibleGames(inputObj)) // NB: we could have used a regex like this const regex = new RegExp(/(\d+) (red|blue|green)/g)
